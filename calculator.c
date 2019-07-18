a8097c02c0db438eb458c07757dbe70d0667e98f
horizonmembers18 <52951727+horizonmembers18@users.noreply.github.com>
2019-07-18 12:29:23 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("Welcome to horizon 2k19");
+int a,b,c,d;
+printf("Enter value of a:",a);
+scanf("%d",&a);
+printf("Enter value of b:",b);
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+printf("The addition is:%d",c);
+printf("The subtraction is:%d",d);
+getch();
+}
+
+
+
