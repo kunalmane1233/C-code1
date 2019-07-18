1a6ea98c78b86d1ab90dbea59e5b8572501e15c9
kunalmane1233 <53033810+kunalmane1233@users.noreply.github.com>
2019-07-18 13:27:40 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,17 +3,19 @@
 void main()
 {
 printf("Welcome to horizon 2k19");
-int a,b,c,d,e;
-printf("Enter value of a:",a);
+int a,b,c,d,e,d;
+printf("\nEnter value of a:",a);
 scanf("%d",&a);
-printf("Enter value of b:",b);
+printf("\nEnter value of b:",b);
 scanf("%d",&b);
 c=a+b;
 d=a-b;
 e=a*b;
-printf("The addition is:%d",c);
-printf("The subtraction is:%d",d);
-printf("The multiplication is:%d",e);
+f=a/b;
+printf("\nThe addition is:%d",c);
+printf("\nThe subtraction is:%d",d);
+printf("\nThe multiplication is:%d",e);
+printf("\nThe division is:%d",f);
 getch();
 }
 
